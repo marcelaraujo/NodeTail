da5cbb25998e3f3e226f20242fab28ff138eaace
marcelaraujo <ceceldada@gmail.com>
2013-01-16 16:22:30 -0200

    Wrapping namespace

--- nodetail.cc
+++ nodetail.cc
@@ -10,26 +10,29 @@
 using namespace node;
 using namespace v8;
 
-Handle<Value> Add(const Arguments& args) {
-	HandleScope scope;
-   
-	if (args.Length() < 2) {
-		ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-		return scope.Close(Undefined());
+namespace {
+	Handle<Value> Add(const Arguments& args) {
+		HandleScope scope;
+
+		if (args.Length() < 2) {
+			ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
+			return scope.Close(Undefined());
+		}
+
+		if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+			ThrowException(Exception::TypeError(String::New("Wrong arguments")));
+			return scope.Close(Undefined());
+		}
+
+		Local<Number> num = Number::New(args[0]->NumberValue() + args[1]->NumberValue());
+
+		return scope.Close(num);
 	}
 
-	if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
-		ThrowException(Exception::TypeError(String::New("Wrong arguments")));
-		return scope.Close(Undefined());
+	extern "C" void Init(Handle<Object> target) {
+		HandleScope scope;
+		target->Set(String::NewSymbol("add"), FunctionTemplate::New(Add)->GetFunction());
 	}
-
-	Local<Number> num = Number::New(args[0]->NumberValue() + args[1]->NumberValue());
 	
-	return scope.Close(num);
+	NODE_MODULE(nodetail, Init)
 }
-
-void Init(Handle<Object> target) {
-	target->Set(String::NewSymbol("add"), FunctionTemplate::New(Add)->GetFunction());
-}
-
-NODE_MODULE(nodetail, Init)
--- tests/test01.js
+++ tests/test01.js
@@ -1,3 +1,7 @@
-var addon = require('./../build/Release/yeap.node');
+var addon = require('./../build/Release/nodetail.node');
+
+addon.context.on = function(data) {
+	console.log(data);
+};
 
 console.log( 'This should be eight:', addon.add(3,5) );
